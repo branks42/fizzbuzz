70e597dc0d444f51b91377b163c9bb480314c636
Justin Branco <justin.brancode@gmail.com>
2018-05-02 11:59:10 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -25,5 +25,16 @@ for num in range(1, 21):
 		print('Buzz')
 	else:
 		print(num)
+# A 3rd option
+for num in range(1, 21):
+	string = ''
+	if num % 3 == 0:
+		string = string + 'Fizz'
+	if num % 5 == 0:
+		string = string + 'Buzz'
+	if num % 5 != 0 and num % 3 != 0:
+		string = string + str(num)
+	print(string, end=' ')
+
 
 # END.
